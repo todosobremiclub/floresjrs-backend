32269b9f5517d431b740f08df0fc635423e0818c
Leonardo Sardella <lsardella@securitasargentina.com>
2025-06-17 11:56:36 -0300

    Usar secret file de Render para credenciales de Firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,18 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'floresjrs-fcd20.appspot.com',
-});
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT;
+if (!serviceAccountPath) {
+  throw new Error('🚨 Falta configurar FIREBASE_SERVICE_ACCOUNT');
+}
+
+// Carga las credenciales desde la ruta del Secret File
+const serviceAccount = require(serviceAccountPath);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'floresjrs-fcd20.appspot.com',
+  });
+}
 
 module.exports = admin;
