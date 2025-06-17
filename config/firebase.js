54791f1ac96e7a825d050a874992f9b7cb6a4498
Leonardo Sardella <lsardella@securitasargentina.com>
2025-06-17 13:50:30 -0300

    Corregir bucket a floresjrs-b43c7.firebasestorage.app

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,10 @@
 const admin = require('firebase-admin');
-
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT;
-if (!serviceAccountPath) {
-  throw new Error('🚨 Falta configurar FIREBASE_SERVICE_ACCOUNT');
-}
-
-// Carga las credenciales desde la ruta del Secret File
-const serviceAccount = require(serviceAccountPath);
+const serviceAccount = require('./firebase-service-account.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'floresjrs-fcd20.appspot.com',
+    storageBucket: 'floresjrs-b43c7.firebasestorage.app'
   });
 }
 
