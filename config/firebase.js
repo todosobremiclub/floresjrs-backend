0662b8adb347dde9a691a73b1f30cae5f93f6be5
Leonardo Sardella <lsardella@securitasargentina.com>
2025-06-24 18:08:34 -0300

    Migrar Firebase a autenticación con variable base64

--- config/firebase.js
+++ config/firebase.js
@@ -1,18 +1,11 @@
 const admin = require('firebase-admin');
 
+const base64 = process.env.FIREBASE_CREDENTIAL_BASE64;
+const json = Buffer.from(base64, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(json);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  }),
+  credential: admin.credential.cert(serviceAccount),
   storageBucket: 'floresjrs-b6d5e.appspot.com'
 });
 
