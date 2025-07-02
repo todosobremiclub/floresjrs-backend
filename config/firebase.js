0c3a216d955d87fb6889fb294b9e3c1116030ad8
Leonardo Sardella <lsardella@securitasargentina.com>
2025-07-02 11:21:08 -0300

    Usar credenciales de Firebase desde variable de entorno segura en Render

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./floresjrs-b6d5e-firebase-adminsdk-fbsvc-f7ff4d662d.json');
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'floresjrs-b6d5e.appspot.com' // 📦 necesario para usar storage
+  storageBucket: 'floresjrs-b6d5e.appspot.com' // 🔁 Reemplazá por tu bucket real
 });
 
 module.exports = admin;
+
 	
--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./floresjrs-b6d5e-firebase-adminsdk-fbsvc-f7ff4d662d.json'); // Usa el nombre exacto de tu archivo
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'floresjrs-b6d5e.appspot.com' // 🔁 Reemplazá por tu bucket real
 });
 
 module.exports = admin;
