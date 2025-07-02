34c25083eed6574e98670e609272c88d3e2265fb
Leonardo Sardella <lsardella@securitasargentina.com>
2025-07-02 11:24:29 -0300

    Evitar doble inicialización de Firebase Admin con apps.length

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,12 @@
 const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'floresjrs-b6d5e.appspot.com' // 🔁 Reemplazá por tu bucket real
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'floresjrs-b6d5e.appspot.com' // Reemplazá si aplica
+  });
+}
 
 module.exports = admin;
 
